c2b37f3f02c2e5b6e1239d363a61ea296ca16c9d
Aksel Sjögren <asjogren@itrsgroup.com>
2020-02-11 13:33:26 +0100

    Run feature test suite with UNIX/TCP connections
    To make sure that Livestatus works over a TCP connection as well as a
    UNIX socket, this change will run the feature test suite with Livestatus
    configured to listen on a TCP socket as well.
    
    - Run the whole test suite twice and load Livestatus with TCP config if
    'LIVESTATUS_TCP' environment variable is set.
    
    - Add TCP support to the ls-clobber.py script and let it know what mode
    to connect with.
    
    - Fix the most serious things pylint complained about in ls-clobber.py.
    
    - Add ls-query.py script to use for querying Livestatus over TCP for the
    feature test suite.
    
    - Set PATH instead of custom paths for each test tool.
    
    Signed-off-by: Aksel Sjögren <asjogren@itrsgroup.com>

--- tests/tools/ls-clobber.py
+++ tests/tools/ls-clobber.py
@@ -35,29 +35,33 @@ def receive_response(s):
 	data = receive_data(s, length)
 	return data
 
-def run_query(q, query='', socket_path='', idle_time_s=0):
+def run_query(q, query='', unix_socket=None, tcp_port=None, idle_time_s=0):
 	if not query.endswith('\n'):
 		query.append('\n')
 	start_t = time.time()
-	with closing(socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)) as s:
-		s.connect(socket_path)
+	with closing(socket.socket(
+			socket.AF_UNIX if unix_socket else socket.AF_INET,
+			socket.SOCK_STREAM)) as s:
+		s.connect(unix_socket or ('localhost', tcp_port))
 		s.send(query)
 		s.send('KeepAlive: on\n')
 		#fixed16 so that we can read the entire message without relying on
 		# socket close
 		s.send('ResponseHeader: fixed16\n\n')
-		data = receive_response(s)
+		# Receive response from Livestatus, although the response data is not used here.
+		receive_response(s)
 		time.sleep(idle_time_s)
 	end_t = time.time()
 	q.put(end_t - start_t)
 
-def clobber(socket_path, q, num_threads = 1, idle_time = 1, query = 'GET services\n'):
-	threads=[]
+def clobber(q, unix_socket=None, tcp_port=None, num_threads=1, idle_time=1, query='GET services\n'):
+	threads = []
 	start_t = time.time()
-	for i in range(num_threads):
+	for _ in range(num_threads):
 		th_args = {
 			'idle_time_s': idle_time,
-			'socket_path': socket_path,
+			'unix_socket': unix_socket,
+			'tcp_port': tcp_port,
 			'query': query
 			}
 		threads.append(
@@ -71,7 +75,7 @@ def clobber(socket_path, q, num_threads = 1, idle_time = 1, query = 'GET service
 	end_t = time.time()
 	return end_t - start_t
 
-if __name__ == '__main__':
+def main():
 	import argparse
 	from os import path
 	import json
@@ -83,12 +87,19 @@ if __name__ == '__main__':
 		return string
 
 	parser = argparse.ArgumentParser(description='Clobber Livestatus with concurrent queries for benchmarking.')
-	parser.add_argument('socket', type=existing_path, help='path to the Livestatus UNIX socket')
-	parser.add_argument('nthreads', type=int, help ='the number of concurrent threads to spawn - one thread = one query')
+	parser.add_argument('nthreads', type=int, help='the number of concurrent threads to spawn - one thread = one query')
 	parser.add_argument('idle_time', type=int, help='the time (in seconds) that a thread keeps its socket open before terminating')
+	parser_socket = parser.add_mutually_exclusive_group(required=True)
+	parser_socket.add_argument('--unix-socket', type=existing_path, help='path to the Livestatus UNIX socket')
+	parser_socket.add_argument('--tcp-port', type=int, help='TCP port for inet socket')
 	args = parser.parse_args()
 	q = Queue()
-	total_time = clobber(args.socket, q, num_threads=args.nthreads, idle_time=args.idle_time)
+	total_time = clobber(
+		q,
+		unix_socket=args.unix_socket,
+		tcp_port=args.tcp_port,
+		num_threads=args.nthreads,
+		idle_time=args.idle_time)
 
 	results = []
 	while not q.empty():
@@ -104,3 +115,7 @@ if __name__ == '__main__':
 				'min_time': min_time,
 				'avg_time': avg_time
 				}))
+
+
+if __name__ == '__main__':
+	main()
